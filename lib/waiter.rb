66b51518791ede05b488d4383edf73f17badc1b4
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-25 14:47:21 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -21,11 +21,10 @@ class Waiter
       meal.waiter == self
     end
   end
-  
-  def waiters
-    meal.map do |meal|
-      meal.waiter
+    
+  def best_tipper
+    best_tipped_meal = meals.max do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
     end
-  end
   
 end
